9d5802f342176d5569d20093ece7c402ef2b355d
Звіт Лабораторна робота №5 <65448852+KarinaOrendarchuk@users.noreply.github.com>
2020-10-22 22:53:16 +0300

    Add files via upload

--- 5_2_B.c
+++ 5_2_B.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 int main(void) {
 
@@ -8,7 +9,6 @@ int min_7 = 0; // числа менші за 7
 int dod_1 = -1; // перше додатне число
 int dod_2 = -1; // друге додатне число
 int summ = 0;  // сума елементів від 1 до останнього додатного числа
-char not_dod = 0; //якшо немає додатніх чисел
 int Low = -100, High = 100;
 srand(time(0));
 
@@ -23,11 +23,6 @@ for (int i = 0; i < n; i++)
 	a[i]=Low+rand()%(High - Low + 1);
 	if (a[i] < 7)
 	{
-		min_7+1;
-	}
-
-	if (a[i] < 7)
-	{
 		min_7++;
 	}
 	if (a[i] > 0 && dod_1 == -1)
@@ -40,20 +35,6 @@ for (int i = 0; i < n; i++)
 	}
 }
 
-if (dod_1 != -1 && dod_2 != -1)
-{
-
-for (; dod_1 <= dod_2; dod_1++)
-		{
-			summ += a[dod_1];
-		}
-
-}else	if (dod_1 != -1 && dod_2 == -1)
-		{
-			summ = a[dod_1];
-		} else not_dod = 1;
-
-
 printf("\n\nМасив:\n");
 for (int i = 0; i < n; i++)
 {	
@@ -61,10 +42,24 @@ for (int i = 0; i < n; i++)
 }
 
 printf("\n1) кількість елементів масиву,\n   менших за число сім: %d\n", min_7);
-if (not_dod == 1)
+
+if (dod_1 == -1 && dod_2 == -1)
 {
 	printf("\n2) В масиві немає додатних чисел\n");
-} else printf("\n2) Сума елементів масиву,\n   розташованих між першим й останнім\n   додатними елементами: %d\n", summ);
+}
+	else if (dod_1 == dod_2)
+	{
+		printf("\n2) В масиві тільки одне додатне чисело\n");
+	} 
+		else if (a[dod_1] != -1 && a[dod_2] != -1)
+		{
+
+			for (int i = dod_1; i <= dod_2; i++)
+			{
+				summ += a[i];
+			}
+		printf("\n2) Сума елементів масиву,\n   розташованих між першим й останнім\n   додатними елементами: %d\n", summ);
+		}
 
 return 0;
 }
